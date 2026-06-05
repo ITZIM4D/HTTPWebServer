fe31dce8a2be86d0374d07aedf6454d90f1324dd
Jared Wilson <wilson.jared603@gmail.com>
2026-06-04 22:14:12 -0400

    Created initial threadpool header and cpp file

---
+++ include/ThreadPool.h
@@ -0,0 +1,18 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <thread>
+
+using std::thread;
+
+// Template function for addThread function parameter
+template <typename T> T
+
+class ThreadPool {
+    public:
+        void addThread() 
+    private:
+        std::vector<thread> threadPool;
+}
+
+#endif
