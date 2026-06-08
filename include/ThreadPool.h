b11fe5f4b724df84888276734b006f38ee7a0b68
Jared Wilson <wilson.jared603@gmail.com>
2026-06-08 10:18:31 -0400

    Added doxygen style comments to ThreadPool header file

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,3 +1,9 @@
+/**
+ * @file ThreadPool.h
+ *
+ * @brief A pool of threads constantly running and taking open jobs
+ */
+
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
@@ -7,22 +13,46 @@
 using std::thread;
 
 /**
+ * @class ThreadPool
+ *
  * @brief A thread pool that will allocate threads to tasks
  */
 class ThreadPool {
     public:
+        /**
+         * @brief Creates the threads and starts the threadpool loop
+         */
         void start();
+        
+        /**
+         * @brief Queues up a job for one of the threads to take
+         *
+         * @param[in] job The job that is being queued up
+         */
         void queueJob(const std::function<void()>& job);
+        
+        /**
+         * @brief Stops the Threadpool and joins the threads
+         */
         void stop();
+
+        /**
+         * @brief Checks and returns if there is a job running or not
+         *
+         * @return True if there is a job in the queue and False if not 
+         */
         bool busy();
 
     private:
         bool shouldTerminate = false; 
-        std::mutex queueMutex;
-        std::condition_variable mutexCondition; 
+        std::mutex queueMutex; /// Mutex that all the threads refer to 
+        std::condition_variable mutexCondition; /// Wake condition for threads  
         std::vector<std::thread> threads;
         std::queue<std::function<void()>> jobs;
 
+        /**
+         * @brief Loop that threads run through and check/wait for a job to open up
+         */
         void threadLoop();
 };
 
