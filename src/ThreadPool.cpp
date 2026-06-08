8c3471602c058e6a061e1357b4d78e15990d4fd1
Jared Wilson <wilson.jared603@gmail.com>
2026-06-07 22:12:31 -0400

    Added more to thread pool

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -5,6 +5,30 @@ ThreadPool::ThreadPool(int poolSize) {
     for (int i = 0; i < poolSize; i++) {
         threadPool.emplace_back(thread(&ThreadPool::ThreadLoop, this));
     }
-
 }
 
+void ThreadPool::threadLoop() {
+    while (true) {
+        std::function<void()> job;
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            mutex_condition.wait(lock, []{
+                return !jobs.empty() || should_terminate;
+            });
+            if (should_terminate) {
+                return;
+            }
+            job = jobs.front();
+            jobs.pop();
+        }
+        job();
+    }
+}
+
+void ThreadPool::queueJob(const std::function<void()>& job) {
+    {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    jobs.push(job);
+    }
+    mutex_condition.notify_one();
+}
